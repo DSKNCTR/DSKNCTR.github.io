8a8fa9b5b4691d842c6dc6fc5919b8584d7ebe17
NCTR7 <random.ai.xxx@gmail.com>
2023-08-31 00:07:17 +0530

    NewV

--- README.md
+++
@@ -1,2 +0,0 @@
-# DSKNCTR.github.io
-NCTR7 - Portfolio
---
+++ script.js
@@ -0,0 +1,11 @@
+// function changeslash() {
+//     var item = document.getElementById('slash').children;
+//     item.innerHTML = ('// ');
+// }
+
+// function Cursor1(){
+//     document.body.style.cursor="url('/main/cursor1.svg'), default";
+// }
+// function Cursor2(){
+//     document.body.style.cursor="url('cursor2.svg'), default";
+// }
